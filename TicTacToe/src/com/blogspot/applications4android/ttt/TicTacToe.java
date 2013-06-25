0f8a4dcaee81f3f7cbb01b72a4fd20525df37962
Tejaswi <rao.thejaswi@gmail.com>
2013-06-25 22:10:02 +0530

    Renamed the main activity class

--- TicTacToe/src/com/blogspot/applications4android/ttt/TicTacToe.java
+++ TicTacToe/src/com/blogspot/applications4android/ttt/TicTacToe.java
@@ -3,7 +3,7 @@ package com.blogspot.applications4android.ttt;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class TicTacToeActivity extends Activity {
+public class TicTacToe extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
