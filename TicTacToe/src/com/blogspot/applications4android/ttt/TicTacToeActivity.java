9d772d224bd18c3d710fdc6571db6351a1dc7ee9
Tejaswi <rao.thejaswi@gmail.com>
2013-06-25 21:26:44 +0530

    Initial files for the project including test project

---
+++ TicTacToe/src/com/blogspot/applications4android/ttt/TicTacToeActivity.java
@@ -0,0 +1,13 @@
+package com.blogspot.applications4android.ttt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TicTacToeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
