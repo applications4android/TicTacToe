0154623b4f43fb4e9cae8dbc28d903ec627cdd8d
Tejaswi <rao.thejaswi@gmail.com>
2013-06-26 09:38:12 +0530

    Added support for automated build using ant

--- scripts/run_unit_tests.sh
+++ scripts/run_unit_tests.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 # COMMENTS:
-#   Simple script to run android junit tests for this project
+#   Simple script to:
+#  . clean/build the project
+#  . clean/build the test project
+#  . Launch an emulator
+#  . run android junit tests for this project
 #
 # USAGE:
 #   ./scripts/run_unit_tests.sh
+#                   [-h, -help, --help]
+#                     [-clean, -no-build]
+#  -h, -help, --help   Print this help and exit
+#  -clean              Clean the projects and build them from scratch
+#  -no-build           Do not build the projects
+#  -avd <avd>          Launch an emulator with this AVD. By default, this script
+#                      assumes that there's already an emulator (and one and only
+#                      one!) running.
 #
 # PRE-REQS:
 #  . Needs an emulator to be running already
 #
+#END
 
 
+
+function showHelp() {
+    sed -e '/#END/q' $0
+    exit 0
+}
 
 function fatalError() {
     local msg=$1
@@ -21,26 +39,63 @@ function fatalError() {
 function runCmd() {
     local cmd=$1
     echo "Running:  $cmd"
-    local status=$?
     $cmd
+    local status=$?
     if [ "$status" != "0" ]; then
         fatalError "Command '$cmd' failed! See above for reasons." $status
     fi
 }
 
+function compileAndInstall() {
+    local dir=$1
+    local clean=$2
+    local apk="$dir/bin/$dir-debug.apk"
+    local buildfile="$dir/build.xml"
+    if [ ! -e "$buildfile" ]; then
+        fatalError "The buildfile '$buildfile' does not exist!"
+    fi
+    if [ "$clean" = "1" ]; then
+        runCmd "ant -buildfile $buildfile clean"
+    fi
+    runCmd "ant -buildfile $buildfile debug"
+    runCmd "adb -e install -r $apk"
+}
 
 
 
-apk="TicTacToe/bin/TicTacToe.apk"
-testApk="TicTacToeTest/bin/TicTacToeTest.apk"
+
 package=com.blogspot.applications4android.ttt.test
 inst=android.test.InstrumentationTestRunner
+cleanProjects=0
+build=1
+avd=
 
-echo "Running the unit-tests..."
+while [ "$1" != "" ]; do
+    case "$1" in
+        "-h" | "-help" | "--help")
+            showHelp;;
+        "-clean")
+            cleanProjects=1
+            shift;;
+        "-no-build")
+            build=0
+            shift;;
+        "-avd")
+            shift
+            avd=$1
+            shift;;
+        *)
+            fatalError "Unknown option passed! '$1'";;
+    esac
+done
+
+if [ "$avd" != "" ]; then
+    runCmd "emulator -avd $avd"
+fi
 runCmd "adb start-server"
 runCmd "adb wait-for-device"
-runCmd "adb -e install -r $apk"
-runCmd "adb -e install -r $testApk"
+if [ "$build" = "1" ]; then
+    compileAndInstall "TicTacToe" $cleanProjects
+    compileAndInstall "TicTacToeTest" $cleanProjects
+fi
 runCmd "adb -e shell am instrument -w ${package}/${inst}"
-echo "Successfully completed the unit-tests..."
-echo
